51d5b0d9061762dee0b863aceab9ceb1baa61d3a
sharonyouchunxue <cyou8@myseneca.ca>
2022-09-11 19:02:27 -0400

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Chunxue You - 127632214");
 });
 
 // setup http server to listen on HTTP_PORT
