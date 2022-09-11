653cb5e2d6692cc87ec8ddc7620a2f0280265738
sharonyouchunxue <cyou8@myseneca.ca>
2022-09-11 19:08:21 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Chunxue You Student ID: 127632214  Date: 2022-09-11
+*
+*  Online (Cyclic) URL: _______________________________________________________
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
